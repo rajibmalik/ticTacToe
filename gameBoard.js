91c29922495f3c2edf994aef8b3f62f91e6dbad5
rajibmalik <rajibmalik@hotmail.co.uk>
2024-12-10 14:57:02 +0000

    Created dynamic gameBoard class

---
+++ gameBoard.js
@@ -0,0 +1,34 @@
+export class gameBoard {
+  #board;
+  #rowLength;
+  #columnLength;
+
+  constructor(row, column) {
+    this.#board = new Array(row);
+
+    for (let i = 0; i < this.#board.length; i++) {
+      this.#board[i] = new Array(column).fill(0);
+    }
+
+    this.#rowLength = row;
+    this.#columnLength = column;
+  }
+
+  getBoard() {
+    return this.#board;
+  }
+
+  updateCell(row, column, value) {
+    if (row >= this.#rowLength || row < 0) {
+      console.log("Invalid row");
+      return;
+    }
+
+    if (column >= this.#columnLength || column < 0) {
+      console.log("Invalid column");
+      return;
+    }
+
+    this.#board[row][column] = value;
+  }
+}
